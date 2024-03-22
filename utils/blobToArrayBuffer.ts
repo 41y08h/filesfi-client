1e3b2221164f81f5a52b207df659375231af159d
Piyush <piyush41y08h@gmail.com>
2024-03-22 11:16:11 +0530

    refactor `blobToArrayBuffer()`

--- utils/blobToArrayBuffer.ts
+++ utils/blobToArrayBuffer.ts
@@ -1,12 +1,13 @@
 export default async function blobToArrayBuffer(
   blob: Blob
 ): Promise<ArrayBuffer> {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.readAsArrayBuffer(blob);
     reader.addEventListener("load", (event) => {
       const chunk = event.target.result as ArrayBuffer;
       resolve(chunk);
     });
+    reader.addEventListener("error", (ev) => reject(ev.target.error));
+    reader.readAsArrayBuffer(blob);
   });
 }
